61a75e3ef96a8ddb1354f8baff5d96f8828db2c6
Kabuto <neokabuto@gmail.com>
2014-07-14 18:43:23 -0400

    Starting XML export

--- js/create.js
+++ js/create.js
@@ -42,6 +42,8 @@ $(function(){
 	$("#exportbtn").click( function(){
 		//create XML output
 		$("#xmloutput").slideDown();
+		$("#xmloutput").html("<code id=\"xmlcode\"></code>");
+		$("#xmlcode").text(toXML());
 	});
 });
 
@@ -53,4 +55,87 @@ var listOptions = function () {
 	});
 	
 	return list;
+};
+
+var updatePrereqs = function() {
+	var prereqSelects = $("select.prereq");
+	var opts = $(".option");
+	
+	$(prereqSelects).each(function (index, element){
+		
+		$(element).empty().append("<option>None Required</option>");
+		
+		$(opts).each(function (optindex, optelement){
+			
+		});
+		
+	});
+}
+
+var toXML = function() {
+	var output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><doc>";
+	
+	//write settings
+	output += "<settings>";
+	output += "<setting key=\"Title\">";
+	output += $("#inputTitle").val();
+	output += "</setting>";
+	output += "<setting key=\"StartPoints\">";
+	output += parseInt($("#inputPoints").val());
+	output += "</setting>";
+	output += "<setting key=\"HeaderImage\">";
+	output += $("#inputHeader").val();
+	output += "</setting>";
+	output += "</settings>";
+	
+	
+	//write categories
+	var cats = $(".category:not(#samplecategory)");
+	
+	output += "<categories>";
+	
+	
+	$(cats).each(function(index, element){
+		output += "<category id=\""+ $(element).attr("id").substring(4) +"\">";
+		
+		output += "<name>";
+		output += $(element).find("#inputTitle").val();
+		output += "</name>";
+		
+		output += "<description>";
+		output += $(element).find("#inputDescription").val();
+		output += "</description>";
+		
+		output += "<image>";
+		output += $(element).find("#inputHeader").val();
+		output += "</image>";
+		
+		if($(element).find("#inputPrereq").val() !== "None Required"){
+			output += "<prereq>";
+			output += $(element).find("#inputPrereq").val();
+			output += "</prereq>";
+		}
+		
+		output += "<starthidden>";
+		output += $(element).find("#inputStartHidden").val();
+		output += "</starthidden>";
+		
+		output += "<maxchoices>";
+		output += $(element).find("#inputMaxChoices").val();
+		output += "</maxchoices>";
+		
+		//write options
+		var opts = $(cats).find(".option");
+		$(opts).each(function(oindex, oelement){
+			output += "<option id=\""+ $(element).attr("id").substring(4) +"\">";
+			output += "</option>";			
+		});
+		
+		output += "</category>";
+		
+		});
+	output += "</categories>";
+	
+	output += "</doc>";
+	return output;
 };
